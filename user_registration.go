6705ee5026f9c134520c82d33a06f76a47482404
egaevan <ega.evan@salt.co.id>
2025-12-17 11:16:28 +0700

    feat: [ega] Add user registration - valid case - no error without invalid unit test

--- user_registration.go
+++ user_registration.go
@@ -1,1 +1,15 @@
 package authentication
+
+type User struct {
+	email    string
+	password string
+	status   string
+}
+
+func (u User) Status() string {
+	return u.status
+}
+
+func Register(email, password string) (*User, error) {
+	return &User{}, nil
+}
