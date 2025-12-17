54ea4e933d0da567680e0acd090162841f1dbb1d
egaevan <ega.evan@salt.co.id>
2025-12-17 11:11:45 +0700

    feat: [ega] Add user registration - valid case

---
+++ user_registration_test.go
@@ -0,0 +1,15 @@
+package authentication
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestRegistration_Valid(t *testing.T) {
+	email := "testing@gmail.com"
+	password := "testing123"
+	user, err := Register(email, password)
+	assert.Equal(t, nil, err)
+	assert.NotEqual(t, password, user.password)
+	assert.Equal(t, "active", user.status)
+}
