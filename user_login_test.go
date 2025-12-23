694900aea7ad8531f8c298ea739fcd83519d36cf
egaevan <ega.evan@salt.co.id>
2025-12-23 09:50:57 +0700

    fix: [ega] Add user login - valid case

--- user_login_test.go
+++ user_login_test.go
@@ -1,6 +1,7 @@
 package authentication
 
 import (
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -12,4 +13,13 @@ func TestLogin_Valid(t *testing.T) {
 	token, err := Login(email, password)
 	assert.Equal(t, nil, err)
 	assert.NotNil(t, token)
+}
+
+func TestLogin_InvalidPassword(t *testing.T) {
+	email := "testing@gmail.com"
+	password := "Testing123#"
+
+	token, err := Login(email, password)
+	assert.Equal(t, errors.New("invalid password"), err)
+	assert.Nil(t, token)
 }
