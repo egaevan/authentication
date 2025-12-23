f292c047596423c770c47a1268c1da0336a912e6
egaevan <ega.evan@salt.co.id>
2025-12-23 11:05:43 +0700

    fix: [ega] Add user logout - user invalid

--- user_logout_test.go
+++ user_logout_test.go
@@ -1,6 +1,7 @@
 package authentication
 
 import (
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -9,4 +10,10 @@ func TestLogout_Valid(t *testing.T) {
 	token := "token"
 	err := Logout(token)
 	assert.Equal(t, nil, err)
+}
+
+func TestLogout_Invalid(t *testing.T) {
+	token := "token-invalid"
+	err := Logout(token)
+	assert.Equal(t, errors.New("invalid token"), err)
 }
