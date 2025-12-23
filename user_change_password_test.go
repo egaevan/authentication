70bb5d870c92520615337cdde2d1643a38bda7b4
egaevan <ega.evan@salt.co.id>
2025-12-23 11:43:32 +0700

    fix: [ega] Add change password - invalid old password

--- user_change_password_test.go
+++ user_change_password_test.go
@@ -1,6 +1,7 @@
 package authentication
 
 import (
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -11,4 +12,12 @@ func TestChangePassword_Valid(t *testing.T) {
 	newPassword := "newPassword"
 	err := ChangePassword(token, oldPassword, newPassword)
 	assert.Equal(t, nil, err)
+}
+
+func TestChangePassword_InvalidOldPassword(t *testing.T) {
+	token := "token"
+	oldPassword := "oldPassword-wrong"
+	newPassword := "newPassword"
+	err := ChangePassword(token, oldPassword, newPassword)
+	assert.Equal(t, errors.New("invalid old password"), err)
 }
