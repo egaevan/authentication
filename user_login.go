cd186d326d04a752521554fedb1c166e5135d3ce
egaevan <ega.evan@salt.co.id>
2025-12-23 09:48:53 +0700

    fix: [ega] Add user login - valid case

--- user_login.go
+++ user_login.go
@@ -1,5 +1,20 @@
 package authentication
 
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+)
+
+var jwtSecret = []byte("secret")
+
 func Login(email string, password string) (token string, err error) {
-	return "token", nil
+	claims := jwt.MapClaims{
+		"email": email,
+		"exp":   time.Now().Add(24 * time.Hour).Unix(),
+		"iat":   time.Now().Unix(),
+	}
+
+	tokenJwt := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return tokenJwt.SignedString(jwtSecret)
 }
