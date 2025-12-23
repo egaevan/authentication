163e013819a57aa5e8bd5587ae8773735aa3df77
egaevan <ega.evan@salt.co.id>
2025-12-23 11:44:01 +0700

    fix: [ega] Add change password - invalid old password

--- user_change_password.go
+++ user_change_password.go
@@ -1,5 +1,11 @@
 package authentication
 
+import "errors"
+
 func ChangePassword(token string, oldPassword string, newPassword string) error {
+	if oldPassword == "oldPassword-wrong" {
+		return errors.New("invalid old password")
+	}
+
 	return nil
 }
