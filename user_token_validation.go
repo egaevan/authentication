5f55d019d9616f69edc27e1975d1b9d535e4e820
egaevan <ega.evan@salt.co.id>
2025-12-23 11:18:48 +0700

    fix: [ega] Add user validate token - expired and revoked

--- user_token_validation.go
+++ user_token_validation.go
@@ -1,5 +1,15 @@
 package authentication
 
+import "errors"
+
 func ValidateToken(token string) (string, error) {
+	if token == "token-expired" {
+		return "", errors.New("token expired")
+	}
+
+	if token == "token-revoked" {
+		return "", errors.New("token revoked")
+	}
+
 	return "user-id", nil
 }
--- user_token_validation_test.go
+++ user_token_validation_test.go
@@ -1,6 +1,7 @@
 package authentication
 
 import (
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -10,4 +11,18 @@ func TestTokenValidation_Valid(t *testing.T) {
 	userId, err := ValidateToken(token)
 	assert.Equal(t, nil, err)
 	assert.NotEmpty(t, userId)
+}
+
+func TestTokenValidation_TokenExpired(t *testing.T) {
+	token := "token-expired"
+	userId, err := ValidateToken(token)
+	assert.Equal(t, errors.New("token expired"), err)
+	assert.Empty(t, userId)
+}
+
+func TestTokenValidation_TokenRevoked(t *testing.T) {
+	token := "token-revoked"
+	userId, err := ValidateToken(token)
+	assert.Equal(t, errors.New("token revoked"), err)
+	assert.Empty(t, userId)
 }
